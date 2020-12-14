60c6d749cdd2fe3235f95a6249e87146c5eb087f
Ariel <ariel.usabilidade@gmail.com>
2020-12-14 20:58:06 -0300

    styles fixed

--- main.js
+++ main.js
@@ -1,23 +1,20 @@
-/* ~~~~~ Funções ~~~~~ */
-const escreveTexto = () => {
-  for(let i=0; i < conteudo.length; i++){
-    textoPrincipal.innerText = conteudo[i]
-  }
-}
 
+/* ~~~~~ Funções ~~~~~ */
 // Adiciona pontos ao placar
 const addPonto = () => {
-  for(let i = 0; i < perguntas.length; i++){
+  for(let i = 0; i < data.perguntas.length; i++){
     resultados.innerText = i
   }
 }
 
-// Mostra as opções se o texto for uma pergunta
+// Mostra as opções se o texto for uma pergunta (desnecessário pra apresentação de hoje)
+/* 
 const esconder = () => form.classList.add("invisivel");
 
 const mostrar = () => form.classList.toggle("invisivel");
 
 const ehUmaPergunta = () => textoPrincipal.innerHTML.indexOf('?') != -1 ? mostrar() : esconder();
+*/
 
 // Loopa os dados e insere no html alterando o DOM só 1 vez
 const criarQuiz = () => {
@@ -30,15 +27,15 @@ const criarQuiz = () => {
 
         respostas.push(
           `<input
-            id='pergunta_${i}' 
             type='radio' 
             name='pergunta_${i}' 
             value='${letra}' 
             class='resposta_${i} resultados' 
-          />
-          <label class='rotulo' for='pergunta_${i}'>
-            ${perguntaAtual.respostas[letra]}
-            </label>`
+            id='resposta_${perguntaAtual.respostas[letra]}' 
+            />
+            <label class='rotulo' for='pergunta_${perguntaAtual.respostas[letra]}'>
+              ${perguntaAtual.respostas[letra]}
+              </label>`
         );
       }
 
@@ -58,19 +55,23 @@ const criarQuiz = () => {
     // Calcula, valida e exibe os resultados do quiz na tela
     quiz.innerHTML = html.join("")
 }
+
 const calcularResultados = () => {
   const containersDeCadaResposta = quiz.querySelectorAll('.respostas');
   let acertos = 0;
 
-  quiz.forEach((perguntaAtual, numeroDaQuestao) => {
+  data.forEach((perguntaAtual, numeroDaQuestao) => {
     
-    const containerDeTodasAsRespostas = containersDeCadaResposta[numeroDaQuestao];
-    const seletor = `input[name=pergunta_${numeroDaQuestao}]:checked`;
-    const respostaDoUsuario = (containerDeTodasAsRespostas.querySelector(seletor) || {}).value;
+    const containerDasRespostas = containersDeCadaResposta[numeroDaQuestao];
+    const seletor = `input[name='pergunta_${numeroDaQuestao}']:checked`;
+    const respostaDoUsuario = (containerDasRespostas.querySelector(seletor) || {}).value;
 
     if(respostaDoUsuario === perguntaAtual.respostaCerta){
       acertos++
+      addPonto()
+    }else{
     }
+    
   });
   // show number of correct answers out of total
   placarTaxa.innerHTML = `${acertos}/${quiz.length}`;
@@ -108,19 +109,16 @@ let quiz = document.querySelector(".quiz.interno");
 let form = document.querySelector("form");
 let textoPrincipal = document.querySelector(".texto.principal");
 let resultados = document.querySelector(".placar.numero");
-let input = document.querySelector("input.opcoes");
 let placarTaxa = document.querySelector(".placar.taxa");
-
-let submit = document.querySelector("button.enviar");
 
 
 /* ~~~~~ Chamada das funções ~~~~~ */
 criarQuiz()
-ehUmaPergunta()
 
 //Paginação
 const botaoVoltar = document.querySelector('.voltar');
 const botaoAvancar = document.querySelector('.avancar');
+let submit = document.querySelector("button.enviar");
 const slides = document.querySelectorAll('.slide');
 let slideAtual = 0;
 
