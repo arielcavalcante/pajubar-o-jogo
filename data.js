66986f830575cc71bbc5ac96cce3ab1beb38d48b
Ariel <ariel.usabilidade@gmail.com>
2020-12-14 20:58:06 -0300

    styles fixed

--- data.js
+++ data.js
@@ -1,22 +1,53 @@
 const data = [
   {
-    perguntas: "Quantos alibãs são necessários pra transformar uma magia em um diag leif?",
+    perguntas: "Quantos alibãs são necessários pra transformar qualquer nait na buati em uma magia OU um diag leife?",
     respostas: {
-      a: "Zero",
-      b: "Um",
-      c: "Dois",
-      d: "Três"
+      a: "• Zero",
+      b: "• Um",
+      c: "• Dois",
+      d: "• Pelo menos 7",
+      e: "• Como é?"
     },
     respostaCerta: "b"
   },
   {
-    perguntas: "Quantos programas certos tu já escreveu, Ariel?",
+    perguntas: "Para efetuar uma quebração-de-louça, do que precisamos?",
     respostas: {
-      a: "Zero",
-      b: "Uma",
-      c: "Duas",
-      d: "Três"
+      a: "• Louça né",
+      b: "• Ferramentas Pesadas",
+      c: "• Duas ou mais passivas",
+      d: "• Grindr ou tinder"
+    },
+    respostaCerta: "c"
+  },
+  {
+    perguntas: "Se eu te falar que vou fazer uma gravação mais tarde, o que farei?",
+    respostas: {
+      a: "• Exercícios Vocais",
+      b: "• Cantar no chuveiro",
+      c: "• LipSync",
+      d: "• B*q**t*"
+    },
+    respostaCerta: "d"
+  },
+  {
+    perguntas: "Eduarda me contou que fez a Katia ontem quando viu o ex no terminal do Antonio Bezerra. Eduarda: ",
+    respostas: {
+      a: "• Fez a Egípcia",
+      b: "• Fez a Elza",
+      c: "• Fez o ex",
+      d: "• Fez a Winona"
     },
     respostaCerta: "a"
+  },
+  {
+    perguntas: "E o que é 'Fazer a Egípcia'?",
+    respostas: {
+      a: "• Dançar É O Tchan",
+      b: "• Passar de ladinho num lugar apertado",
+      c: "• Virar a cara",
+      d: "• Ficar imóvel"
+    },
+    respostaCerta: "d"
   }
 ]
